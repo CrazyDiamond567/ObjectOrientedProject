ce59018e756773fc61da25701e20e8175c660cbb
CrazyDiamond567 <vetaranto@outlook.com>
2017-11-11 22:45:18 -0500

    This is still a broken mess, I will fix it in the morning

--- ObjectOrientedProject/src/Controller.java
+++ ObjectOrientedProject/src/Controller.java
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
-
-public class Controller implements Command {
-	Command slot;
-	
-	public Controller() {}
-	
-	public void setCommand(Command command) {
-		slot = command;
-=======
-//Omeed was here
 public class Controller {
 
 	public static void parseInput(String input) {
 		
 		String[] commandSplit = input.split(" ");
 		
+		public void parseInput(String input) {
+			String[] inputArray = input.split(" ");
+			matchInputToCommand(inputArray);
+		}
+		
 		if (commandSplit[0].equals("test")) {
 			System.out.println(commandSplit[1]);
 		}
->>>>>>> branch 'master' of https://github.com/CrazyDiamond567/ObjectOrientedProject.git
 	}
 	
-	public void execute(String y) {
-		slot.execute(y);
-	}
 }
