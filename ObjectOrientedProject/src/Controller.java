4df2a47060e1aa8156d33cfb49d39d7ea58859aa
CrazyDiamond567 <vetaranto@outlook.com>
2017-11-21 11:34:29 -0500

    Fixed Indentation

--- ObjectOrientedProject/src/Controller.java
+++ ObjectOrientedProject/src/Controller.java
@@ -9,35 +9,35 @@ public class Controller {
 		}
 		else {
 		
-		String[] inputArray = input.split(" ");
-		boolean result = true;
+			String[] inputArray = input.split(" ");
+			boolean result = true;
 			
-		switch (inputArray[0]) {
-			case "help":
-				helper.handleHelp(inputArray[1]);
-				break;
-			case "info":
-				System.out.println("Adventure Game");
-				break;
-			case "move":
-				//switch statements can be allowed to fall through, see link
-				//https://docs.oracle.com/javase/tutorial/java/nutsandbolts/switch.html
-				//I am taking advantage of that behavior here
-			case "m":
-				map.handleMovement(inputArray[1]);
-				break;
-			case "talk":
-			case "t":
-				Room room = map.returnCurrentRoom();
-				room.handleTalk(inputArray[1]);
-				break;
-			case "quit":
-				System.out.println("you have quit the game");
-				System.exit(0);
-				// quit program
-			default:
-				System.out.println("type help for commands");
-		}
+			switch (inputArray[0]) {
+				case "help":
+					helper.handleHelp(inputArray[1]);
+					break;
+				case "info":
+					System.out.println("Adventure Game");
+					break;
+				case "move":
+					//switch statements can be allowed to fall through, see link
+					//https://docs.oracle.com/javase/tutorial/java/nutsandbolts/switch.html
+					//I am taking advantage of that behavior here
+				case "m":
+					map.handleMovement(inputArray[1]);
+					break;
+				case "talk":
+				case "t":
+					Room room = map.returnCurrentRoom();
+					room.handleTalk(inputArray[1]);
+					break;
+				case "quit":
+					System.out.println("you have quit the game");
+					System.exit(0);
+					// quit program
+				default:
+					System.out.println("type help for commands");
+			}
 		}
 	}
 		
